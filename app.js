a58e5ec2b3131c8124f6e243a733ca2dc639c27b
Kishan Kokal <kokalkishan.official@gmail.com>
2023-06-02 14:11:27 +0530

    Manipulate HTML with promises

--- app.js
+++ app.js
@@ -384,10 +384,10 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
 // })
 
 // using async / await
-// async function getData() {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-//     const data = await response.json()
-//     console.log(data)
-// }
+async function getData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    email.innerHTML = data.email
+}
 
-// getData()
+getData()
